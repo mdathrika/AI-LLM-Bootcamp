cddc94e349809e71aad4f16162f5970ef2bc51f8
Dathrika <mdathrika@ebay.com>
2024-09-02 12:37:43 -0700

    Milestone 3

--- app.py
+++ app.py
@@ -16,17 +16,23 @@ model_kwargs = {
 
 @cl.on_message
 async def on_message(message: cl.Message):
-    # Your custom logic goes here...
-    print(f"Received message: {message.content}")
+    # Maintain an array of messages in the user session
+    message_history = cl.user_session.get("message_history", [])
+    message_history.append({"role": "user", "content": message.content})
 
-    response = await client.chat.completions.create(
-        messages=[{"role": "user", "content": message.content}],
-        **model_kwargs
-    )
-    # https://platform.openai.com/docs/guides/chat-completions/response-format
-    response_content = response.choices[0].message.content
+    response_message = cl.Message(content="")
+    await response_message.send()
+    
+    # Pass in the full message history for each request
+    stream = await client.chat.completions.create(messages=message_history, 
+                                                  stream=True, **model_kwargs)
+    async for part in stream:
+        if token := part.choices[0].delta.content or "":
+            await response_message.stream_token(token)
 
-    await cl.Message(
-        content=response_content,
-    ).send()
+    await response_message.update()
+
+    # Record the AI's response in the history
+    message_history.append({"role": "assistant", "content": response_message.content})
+    cl.user_session.set("message_history", message_history)
         
