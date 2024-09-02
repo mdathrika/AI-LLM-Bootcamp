fb394a71e039095d28bf901c6b7e82e549bfee41
Dathrika <mdathrika@ebay.com>
2024-09-02 13:20:51 -0700

    Milestone 4

--- app.py
+++ app.py
@@ -2,16 +2,30 @@ import os
 import chainlit as cl
 import openai
 
-api_key = os.getenv("OPENAI_API_KEY")
+# api_key = os.getenv("OPENAI_API_KEY")
 
-endpoint_url = "https://api.openai.com/v1"
+api_key = os.getenv("RUNPOD_API_KEY")
+runpod_serverless_id = os.getenv("RUNPOD_SERVERLESS_ID")
+
+print(f"api_key: {api_key}")
+print(f"runpod_serverless_id: {runpod_serverless_id}")
+
+# endpoint_url = "https://api.openai.com/v1"
+endpoint_url = f"https://api.runpod.ai/v2/{runpod_serverless_id}/openai/v1"
+
 client = openai.AsyncClient(api_key=api_key, base_url=endpoint_url)
 
 # https://platform.openai.com/docs/models/gpt-4o
+# model_kwargs = {
+#     "model": "chatgpt-4o-latest",
+#     "temperature": 1.2,
+#     "max_tokens": 500
+# }
+
 model_kwargs = {
-    "model": "chatgpt-4o-latest",
-    "temperature": 1.2,
-    "max_tokens": 1000
+    "model": "mistralai/Mistral-7B-Instruct-v0.3",
+    "temperature": 0.3,
+    "max_tokens": 500
 }
 
 @cl.on_message
